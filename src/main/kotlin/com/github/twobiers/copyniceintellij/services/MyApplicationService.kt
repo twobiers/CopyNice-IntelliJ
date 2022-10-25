27404f1eedcc9f5f258f60699200479e4afafc68
GitHub Action <action@github.com>
2022-10-25 16:50:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/twobiers/copyniceintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/twobiers/copyniceintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.twobiers.copyniceintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.twobiers.copyniceintellij.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/twobiers/copyniceintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/twobiers/copyniceintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.twobiers.copyniceintellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.twobiers.copyniceintellij.MyBundle
 
 class MyProjectService(project: Project) {
 
